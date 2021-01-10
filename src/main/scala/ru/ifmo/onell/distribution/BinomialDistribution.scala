0acae77f3c83525c541ba6e297e33903f8c7ee96
Maxim Buzdalov <mbuzdalov@gmail.com>
2021-01-10 13:40:46 +0300

    Move scanner to fields of BinomialDistribution implementations.

--- src/main/scala/ru/ifmo/onell/distribution/BinomialDistribution.scala
+++ src/main/scala/ru/ifmo/onell/distribution/BinomialDistribution.scala
@@ -9,7 +9,8 @@ object BinomialDistribution {
     assert(n * p < Int.MaxValue / 2, s"The product of `n` and `p` is too large (${n * p}), sampling would be infeasible")
 
     new IntegerDistribution {
-      override def sample(rng: Random): Int = withScanner(n, p, rng)
+      private[this] val scanner = BinomialScanner(p)
+      override def sample(rng: Random): Int = withScanner(n, scanner, rng)
       override def minValue: Int = 0
       override val maxValue: Int = if (n > Int.MaxValue) Int.MaxValue else n.toInt
     }
@@ -17,19 +18,19 @@ object BinomialDistribution {
 
   def apply(n: Int, p: Double): IntegerDistribution = {
     new IntegerDistribution {
-      override def sample(rng: Random): Int = withScanner(n, p, rng)
+      private[this] val scanner = BinomialScanner(p)
+      override def sample(rng: Random): Int = withScanner(n, scanner, rng)
       override def minValue: Int = 0
       override val maxValue: Int = n
     }
   }
 
-  private def withScanner(n: Long, p: Double, rng: Random): Int = {
-    val sc = BinomialScanner(p)
-    var idx = sc.offset(rng) - 1
+  private def withScanner(n: Long, scanner: BinomialScanner, rng: Random): Int = {
+    var idx = scanner.offset(rng) - 1
     var result = 0
     while (idx < n) {
       result += 1
-      idx += sc.offset(rng)
+      idx += scanner.offset(rng)
     }
     result
   }
